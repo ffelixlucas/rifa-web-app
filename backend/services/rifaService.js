104b09d0f60ddab67288ce59f9f50840f64d7421
Lucas Felix <lucas.fafx@gmail.com>
2025-06-03 11:18:23 -0300

    feat(rifa): orquestrar regras de negócio e validações no service

--- backend/services/rifaService.js
+++ backend/services/rifaService.js
@@ -1,32 +1,66 @@
 const rifaRepository = require('../repositories/rifaRepository');
 
+async function criarRifaComNumeros(rifaData) {
+  const novaRifa = await rifaRepository.criarRifa(rifaData);
+  await rifaRepository.gerarNumeros(novaRifa.id, novaRifa.totalNumeros);
+  return novaRifa;
+}
+
 async function obterRifaPorId(id) {
   const rifa = await rifaRepository.buscarRifaPorId(id);
-
   if (!rifa) {
     const erro = new Error('Rifa não encontrada');
     erro.status = 404;
     throw erro;
   }
-
   return rifa;
 }
 
 async function obterNumerosPorRifaId(rifaId) {
-    const numeros = await rifaRepository.buscarNumerosPorRifaId(rifaId);
-  
-    if (!numeros || numeros.length === 0) {
-      const erro = new Error('Nenhum número encontrado para esta rifa');
-      erro.status = 404;
-      throw erro;
-    }
-  
-    return numeros;
+  const numeros = await rifaRepository.buscarNumerosPorRifaId(rifaId);
+  if (!numeros || numeros.length === 0) {
+    const erro = new Error('Nenhum número encontrado para esta rifa');
+    erro.status = 404;
+    throw erro;
   }
+  return numeros;
+}
 
-  
-  module.exports = {
-    obterRifaPorId,
-    obterNumerosPorRifaId
-  };
-  
+async function atualizarRifa(id, dados) {
+  const rifaExistente = await rifaRepository.buscarRifaPorId(id);
+  if (!rifaExistente) {
+    const erro = new Error('Rifa não encontrada');
+    erro.status = 404;
+    throw erro;
+  }
+  return await rifaRepository.atualizarRifa(id, dados);
+}
+
+async function excluirRifa(id) {
+  const rifaExistente = await rifaRepository.buscarRifaPorId(id);
+  if (!rifaExistente) {
+    const erro = new Error('Rifa não encontrada');
+    erro.status = 404;
+    throw erro;
+  }
+  await rifaRepository.excluirRifa(id);
+}
+
+async function finalizarRifa(id) {
+  const rifaExistente = await rifaRepository.buscarRifaPorId(id);
+  if (!rifaExistente) {
+    const erro = new Error('Rifa não encontrada');
+    erro.status = 404;
+    throw erro;
+  }
+  return await rifaRepository.finalizarRifa(id);
+}
+
+module.exports = {
+  criarRifaComNumeros,
+  obterRifaPorId,
+  obterNumerosPorRifaId,
+  atualizarRifa,
+  excluirRifa,
+  finalizarRifa,
+};
