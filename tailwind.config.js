5ea2c278783ab97cf25ef72e95e0c35677e928bf
Lucas Felix <lucas.fafx@gmail.com>
2025-06-03 13:48:16 -0300

    feat: integra Tailwind e define rota inicial de login com App.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui todos os arquivos em src/
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
@@ -10,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
