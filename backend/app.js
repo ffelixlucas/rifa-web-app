2d43390eead35045f2705e40f24b3b8a6f6fc1f5
Lucas Felix <lucas.fafx@gmail.com>
2025-06-03 10:42:01 -0300

    feat(core): conectar rotas de autenticação e rota protegida /admin

--- backend/app.js
+++ backend/app.js
@@ -8,4 +8,18 @@ app.use(express.json());
 const rifaRoutes = require('./routes/rifaRoutes');
 app.use('/api', rifaRoutes); // agora /api/rifas/:id funciona
 
+const authRoutes = require('./routes/authRoutes');
+app.use('/api', authRoutes);
+
+const verificarToken = require('./middleware/authMiddleware');
+
+// Rota protegida de exemplo
+app.get('/admin', verificarToken, (req, res) => {
+  res.json({
+    mensagem: `Acesso autorizado! Bem-vindo, ${req.usuario.email}`,
+    usuario: req.usuario
+  });
+});
+
+
 module.exports = app;
